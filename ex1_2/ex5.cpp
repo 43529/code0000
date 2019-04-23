a838f3e1e8bb75b1ae498195063c9430c6a6ede4
e <43529and43529@gmail.com>
2019-04-23 22:59:44 +0800

    exercise about the iostream

---
+++ ex1_2/ex5.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+int main(){
+	std::cout<<"Enter two numbers:"<<std::endl;
+	int v1=0, v2=0;
+	std::cin>>v1>>v2;
+	std::cout<<"The sum of "<<std::endl;
+	std::cout<<v1<<" and "<<std::endl;
+	std::cout<<v2<<" is "<<std::endl;
+	std::cout<<v1+v2<<std::endl;
+	return 0;
+}
